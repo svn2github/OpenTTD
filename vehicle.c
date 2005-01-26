svn2github/OpenTTD

commit 437474849f5e3b643f1e124f3a37f8014ee8018c

- Feature: [autoreplace] server now checks for plane<->helicopter replacement and reject replacement if needed
git-svn-id: http://svn.openttd.org/trunk@1687 6aa0318a-3be1-0310-93fa-89fd2396df07

vehicle.c
@@ -1351,6 +1351,9 @@ int32 CmdReplaceVehicle(int x, int y, uint32 flags, uint32 p1, uint32 p2)
 	// check that it's the vehicle's owner that requested the replace
 	if (!CheckOwnership(v->owner)) return CMD_ERROR;
 
+	// makes sure that we do not replace a plane with a helicopter or vise versa
+	if (HASBIT(AircraftVehInfo(old_engine_type)->subtype, 0) != HASBIT(AircraftVehInfo(new_engine_type)->subtype, 0)) return CMD_ERROR;
+
 
 	switch (v->type) {
 		case VEH_Train:    build_cost = EstimateTrainCost(RailVehInfo(new_engine_type)); break;

commit 559c91c392653d298908ff2534546955d5322599

- Fix: [autoreplace] Cheaters can no longer exploit autoreplace to get vehicles, that's not invented yet - Fix: [autoreplace] Fixed a stupid bug introduced in r1687, that made a crash if anybody tried to autoreplace anything but an aircraft
git-svn-id: http://svn.openttd.org/trunk@1690 6aa0318a-3be1-0310-93fa-89fd2396df07

vehicle.c
@@ -1352,8 +1352,12 @@ int32 CmdReplaceVehicle(int x, int y, uint32 flags, uint32 p1, uint32 p2)
 	if (!CheckOwnership(v->owner)) return CMD_ERROR;
 
 	// makes sure that we do not replace a plane with a helicopter or vise versa
-	if (HASBIT(AircraftVehInfo(old_engine_type)->subtype, 0) != HASBIT(AircraftVehInfo(new_engine_type)->subtype, 0)) return CMD_ERROR;
+	if (v->type == VEH_Aircraft) {
+		if (HASBIT(AircraftVehInfo(old_engine_type)->subtype, 0) != HASBIT(AircraftVehInfo(new_engine_type)->subtype, 0)) return CMD_ERROR;
+	}
 
+	// makes sure that the player can actually buy the new engine. Renewing is still allowed to outdated engines
+	if (!HASBIT(DEREF_ENGINE(new_engine_type)->player_avail, v->owner) && old_engine_type != new_engine_type) return CMD_ERROR;
 
 	switch (v->type) {
 		case VEH_Train:    build_cost = EstimateTrainCost(RailVehInfo(new_engine_type)); break;