svn2github/OpenTTD

commit 2c9f8bf6f4f48072642761188e5fbee9033a0a43

- Fix: now a player can only build a road vehicle in depots he owns (hacked client protection)
git-svn-id: http://svn.openttd.org/trunk@1728 6aa0318a-3be1-0310-93fa-89fd2396df07

roadveh_cmd.c
@@ -118,6 +118,10 @@ int32 CmdBuildRoadVeh(int x, int y, uint32 flags, uint32 p1, uint32 p2)
 
 	if (!IsEngineBuildable(p1, VEH_Road)) return CMD_ERROR;
 
+	if (!IsRoadDepotTile((TileIndex)tile)) return CMD_ERROR;
+
+	if (_map_owner[tile] != _current_player) return CMD_ERROR;
+
 	SET_EXPENSES_TYPE(EXPENSES_NEW_VEHICLES);
 
 	cost = EstimateRoadVehCost(p1);

ai.c
@@ -144,7 +144,7 @@ static int AiChooseTrainToBuild(byte railtype, int32 money, byte flag, TileIndex
 	return best_veh_index;
 }
 
-static int AiChooseRoadVehToBuild(byte cargo, int32 money)
+static int AiChooseRoadVehToBuild(byte cargo, int32 money, TileIndex tile)
 {
 	int best_veh_index = -1;
 	int32 best_veh_cost = 0;
@@ -157,7 +157,7 @@ static int AiChooseRoadVehToBuild(byte cargo, int32 money)
 		if (!HASBIT(e->player_avail, _current_player) || e->reliability < 0x8A3D)
 			continue;
 
-		r = DoCommandByTile(0, i, 0, 0, CMD_BUILD_ROAD_VEH);
+		r = DoCommandByTile(tile, i, 0, 0, CMD_BUILD_ROAD_VEH);
 		if (r != CMD_ERROR && r <= money && r >= best_veh_cost) {
 			best_veh_cost = r;
 			best_veh_index = i;
@@ -218,7 +218,7 @@ static int AiChooseShipToBuild(byte cargo, int32 money)
 static int AiChooseRoadVehToReplaceWith(Player *p, Vehicle *v)
 {
 	int32 avail_money = p->player_money + v->value;
-	return AiChooseRoadVehToBuild(v->cargo_type, avail_money);
+	return AiChooseRoadVehToBuild(v->cargo_type, avail_money, v->tile);
 }
 
 static int AiChooseAircraftToReplaceWith(Player *p, Vehicle *v)
@@ -3173,7 +3173,7 @@ static void AiStateBuildRoadVehicles(Player *p)
 	for(;ptr->mode != 0;ptr++) {}
 	tile = TILE_ADD(p->ai.src.use_tile, ToTileIndexDiff(ptr->tileoffs));
 
-	veh = AiChooseRoadVehToBuild(p->ai.cargo_type, p->player_money);
+	veh = AiChooseRoadVehToBuild(p->ai.cargo_type, p->player_money, tile);
 	if (veh == -1) {
 		p->ai.state = AIS_0;
 		return;

commit 11c7ef26152058ecca1ae1beceaa7fd619337976

Fix: [AInew] Allow DC_QUERY_COST even on tiles with no depot built. This is used by the ai_new for deciding if it can afford the vehicles before actually building the depot. Was broken in r1728. Signed-Off-By: TrueLight ;-)
git-svn-id: http://svn.openttd.org/trunk@1946 6aa0318a-3be1-0310-93fa-89fd2396df07

roadveh_cmd.c
@@ -121,15 +121,16 @@ int32 CmdBuildRoadVeh(int x, int y, uint32 flags, uint32 p1, uint32 p2)
 
 	if (!IsEngineBuildable(p1, VEH_Road)) return CMD_ERROR;
 
-	if (!IsTileDepotType(tile, TRANSPORT_ROAD)) return CMD_ERROR;
-
-	if (_map_owner[tile] != _current_player) return CMD_ERROR;
-
 	SET_EXPENSES_TYPE(EXPENSES_NEW_VEHICLES);
 
 	cost = EstimateRoadVehCost(p1);
 	if (flags & DC_QUERY_COST)
 		return cost;
+
+	/* The ai_new queries the vehicle cost before building the route,
+	 * so we must check against cheaters no sooner than now. --pasky */
+	if (!IsTileDepotType(tile, TRANSPORT_ROAD)) return CMD_ERROR;
+	if (_map_owner[tile] != _current_player) return CMD_ERROR;
 
 	v = AllocateVehicle();
 	if (v == NULL || IsOrderPoolFull())