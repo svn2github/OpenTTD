svn2github/OpenTTD

commit 4e4273c2938af12fdfec767aaa00259cd65b1391

Reset vehicle images on game load. Allows savegames with NewGRFs saved prior to r2868 to load.
git-svn-id: http://svn.openttd.org/trunk@2991 6aa0318a-3be1-0310-93fa-89fd2396df07

vehicle.c
@@ -187,6 +187,14 @@ void AfterLoadVehicles(void)
 	FOR_ALL_VEHICLES(v) {
 		v->first = NULL;
 		if (v->type != 0) {
+			switch (v->type) {
+				case VEH_Train: v->cur_image = GetTrainImage(v, v->direction); break;
+				case VEH_Road: v->cur_image = GetRoadVehImage(v, v->direction); break;
+				case VEH_Ship: v->cur_image = GetShipImage(v, v->direction); break;
+				case VEH_Aircraft: v->cur_image = GetAircraftImage(v, v->direction); break;
+				default: break;
+			}
+
 			v->left_coord = INVALID_COORD;
 			VehiclePositionChanged(v);
 

commit ebd4737e39a4dfa0a5f82e4b8957a25bbc038731

Fix: Reset aircraft images differently. Fixes assertion introduced in r2991.
git-svn-id: http://svn.openttd.org/trunk@2993 6aa0318a-3be1-0310-93fa-89fd2396df07

vehicle.c
@@ -191,7 +191,14 @@ void AfterLoadVehicles(void)
 				case VEH_Train: v->cur_image = GetTrainImage(v, v->direction); break;
 				case VEH_Road: v->cur_image = GetRoadVehImage(v, v->direction); break;
 				case VEH_Ship: v->cur_image = GetShipImage(v, v->direction); break;
-				case VEH_Aircraft: v->cur_image = GetAircraftImage(v, v->direction); break;
+				case VEH_Aircraft:
+					if (v->subtype == 0 || v->subtype == 2) {
+						v->cur_image = GetAircraftImage(v, v->direction);
+						if (v->next != NULL) {
+							v->next->cur_image = v->cur_image;
+						}
+					}
+					break;
 				default: break;
 			}
 