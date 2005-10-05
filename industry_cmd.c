svn2github/OpenTTD

commit cae3cb46074d3f319c48c8b5934762becd62a78d

Get rid of quite some dubious casts, either by using GB(), proper types or just removing them
git-svn-id: http://svn.openttd.org/trunk@3010 6aa0318a-3be1-0310-93fa-89fd2396df07

industry_cmd.c
@@ -934,7 +934,7 @@ static void SetupFarmFieldFence(TileIndex tile, int size, byte type, int directi
 		if (IsTileType(tile, MP_CLEAR) || IsTileType(tile, MP_TREES)) {
 
 			or = type;
-			if (or == 1 && (uint16)Random() <= 9362) or = 2;
+			if (or == 1 && GB(Random(), 0, 16) <= 9362) or = 2;
 
 			or <<= 2;
 			and = (byte)~0x1C;
@@ -1486,9 +1486,9 @@ static void DoCreateNewIndustry(Industry *i, TileIndex tile, int type, const Ind
 	i->town = t;
 	i->owner = owner;
 
-	r = Random() & 0xFFF;
-	i->color_map = (byte)(r >> 8);
-	i->counter = (uint16)r;
+	r = Random();
+	i->color_map = GB(r, 8, 8);
+	i->counter = GB(r, 0, 16);
 	i->cargo_waiting[0] = 0;
 	i->cargo_waiting[1] = 0;
 	i->last_mo_production[0] = 0;
@@ -1626,7 +1626,7 @@ Industry *CreateNewIndustry(TileIndex tile, int type)
 		return NULL;
 
 	/* pick a random layout */
-	it = spec->table[(spec->num_table * (uint16)Random()) >> 16];
+	it = spec->table[(spec->num_table * GB(Random(), 0, 16)) >> 16];
 
 	if (!CheckIfIndustryTilesAreFree(tile, it, type, t))
 		return NULL;

aircraft_cmd.c
@@ -1239,8 +1239,7 @@ static void MaybeCrashAirplane(Vehicle *v)
 		prob = 0x10000 / 20;
 	}
 
-	if ((uint16)Random() > prob)
-		return;
+	if (GB(Random(), 0, 16) > prob) return;
 
 	// Crash the airplane. Remove all goods stored at the station.
 	for(i=0; i!=NUM_CARGO; i++) {

disaster_cmd.c
@@ -977,13 +977,13 @@ static void DoDisaster(void)
 	if (j == 0)
 		return;
 
-	_disaster_initprocs[buf[(uint16)Random() * j >> 16]]();
+	_disaster_initprocs[buf[GB(Random(), 0, 16) * j >> 16]]();
 }
 
 
 static void ResetDisasterDelay(void)
 {
-	_disaster_delay = (int)(Random() & 0x1FF) + 730;
+	_disaster_delay = GB(Random(), 0, 9) + 730;
 }
 
 void DisasterDailyLoop(void)

misc.c
@@ -51,12 +51,12 @@ uint32 t;
 #if defined(RANDOM_DEBUG) && !defined(MERSENNE_TWISTER)
 uint DoRandomRange(uint max, int line, const char *file)
 {
-	return (uint16)DoRandom(line, file) * max >> 16;
+	return GB(DoRandom(line, file), 0, 16) * max >> 16;
 }
 #else
 uint RandomRange(uint max)
 {
-	return (uint16)Random() * max >> 16;
+	return GB(Random(), 0, 16) * max >> 16;
 }
 #endif
 
@@ -71,7 +71,7 @@ uint32 InteractiveRandom(void)
 
 uint InteractiveRandomRange(uint max)
 {
-	return (uint16)InteractiveRandom() * max >> 16;
+	return GB(InteractiveRandom(), 0, 16) * max >> 16;
 }
 
 void SetDate(uint date)

namegen.c
@@ -3,12 +3,13 @@
 #include "stdafx.h"
 #include "openttd.h"
 #include "debug.h"
+#include "macros.h"
 #include "namegen.h"
 #include "table/namegen.h"
 
 static inline uint32 SeedChance(int shift_by, int max, uint32 seed)
 {
-	return ((uint16)(seed >> shift_by) * max) >> 16;
+	return (GB(seed, shift_by, 16) * max) >> 16;
 }
 
 static inline uint32 SeedModChance(int shift_by, int max, uint32 seed)

players.c
@@ -1004,7 +1004,7 @@ int8 SaveHighScoreValueNetwork(void)
 		memset(_highscore_table[LAST_HS_ITEM], 0, sizeof(_highscore_table[0]));
 
 		/* Copy over Top5 companies */
-		for (i = 0; i < lengthof(_highscore_table[LAST_HS_ITEM]) && i < (uint8)count; i++) {
+		for (i = 0; i < lengthof(_highscore_table[LAST_HS_ITEM]) && i < count; i++) {
 			char buf[sizeof(_highscore_table[0]->company)];
 
 			hs = &_highscore_table[LAST_HS_ITEM][i];
@@ -1019,7 +1019,7 @@ int8 SaveHighScoreValueNetwork(void)
 			hs->title = EndGameGetPerformanceTitleFromValue(hs->score);
 
 			// get the ranking of the local player
-			if ((*p_cur)->index == (int8)_local_player)
+			if ((*p_cur)->index == _local_player)
 				player = i;
 
 			p_cur++;

roadveh_cmd.c
@@ -987,7 +987,7 @@ static int PickRandomBit(uint bits)
 			num++;
 	} while (b >>= 1);
 
-	num = ((uint16)Random() * num >> 16);
+	num = GB(Random(), 0, 16) * num >> 16;
 
 	for(i=0; !((bits & 1) && ((int)--num) < 0); bits>>=1,i++);
 	return i;
@@ -1029,8 +1029,8 @@ static int RoadFindPathToDest(Vehicle *v, TileIndex tile, int enterdir)
 	{
 		uint32 r;
 		r = GetTileTrackStatus(tile, TRANSPORT_ROAD);
-		signal = (uint16)(r >> 16);
-		bitmask = (uint16)r;
+		signal  = GB(r, 16, 16);
+		bitmask = GB(r,  0, 16);
 	}
 
 	if (IsTileType(tile, MP_STREET)) {

station_gui.c
@@ -318,7 +318,7 @@ static void DrawStationViewWindow(Window *w)
 	StringID str;
 	uint16 station_id;
 
-	station_id = (uint16)w->window_number;
+	station_id = w->window_number;
 
 	st = GetStation(w->window_number);
 

commit 6284173e552bb5ac21bc65bc7ed64d8b8360d762

-Fix: Fixed industry colour issue introduced in r3010.
git-svn-id: http://svn.openttd.org/trunk@3018 6aa0318a-3be1-0310-93fa-89fd2396df07

industry_cmd.c
@@ -1487,8 +1487,8 @@ static void DoCreateNewIndustry(Industry *i, TileIndex tile, int type, const Ind
 	i->owner = owner;
 
 	r = Random();
-	i->color_map = GB(r, 8, 8);
-	i->counter = GB(r, 0, 16);
+	i->color_map = GB(r, 8, 4);
+	i->counter = GB(r, 0, 12);
 	i->cargo_waiting[0] = 0;
 	i->cargo_waiting[1] = 0;
 	i->last_mo_production[0] = 0;