svn2github/OpenTTD

commit b1acddd39a131571cf4f0f9eb9e0b9e4e31a5f14

-Codechange: Replace explicit shifting/anding/oring with GB and SB
git-svn-id: http://svn.openttd.org/trunk@3019 6aa0318a-3be1-0310-93fa-89fd2396df07

rail_cmd.c
@@ -300,8 +300,7 @@ int32 CmdBuildSingleRail(int x, int y, uint32 flags, uint32 p1, uint32 p2)
 
 					if (flags & DC_EXEC) {
 						SetTileOwner(tile, _current_player);
-						_m[tile].m3 &= ~0x0F;
-						_m[tile].m3 |= p1;
+						SB(_m[tile].m3, 0, 4, p1);
 						_m[tile].m5 = (m5 & 0xC7) | 0x20; // railroad under bridge
 					}
 					break;
@@ -322,7 +321,7 @@ int32 CmdBuildSingleRail(int x, int y, uint32 flags, uint32 p1, uint32 p2)
 			}
 			if (m5 & RAIL_TYPE_SPECIAL ||
 					!IsTileOwner(tile, _current_player) ||
-					(_m[tile].m3 & 0xFU) != p1) {
+					GB(_m[tile].m3, 0, 4) != p1) {
 				// Get detailed error message
 				return DoCommandByTile(tile, 0, 0, flags, CMD_LANDSCAPE_CLEAR);
 			}
@@ -453,11 +452,7 @@ int32 CmdRemoveSingleRail(int x, int y, uint32 flags, uint32 p1, uint32 p2)
 			_m[tile].m5 = _m[tile].m5 & 0xC7;
 			break;
 		case MP_STREET:
-			if (!(_m[tile].m5 & 0xF0))
-				return CMD_ERROR;
-
-			if (_m[tile].m5 & 0xE0)
-				return CMD_ERROR;
+			if (!IsLevelCrossing(tile)) return CMD_ERROR;
 
 			/* This is a crossing, let's check if the direction is correct */
 			if (_m[tile].m5 & 8) {
@@ -793,8 +788,7 @@ int32 CmdBuildSingleSignal(int x, int y, uint32 flags, uint32 p1, uint32 p2)
 				if (pre_signal) {
 					// cycle between normal -> pre -> exit -> combo -> pbs ->...
 					byte type = ((GetSignalType(tile, track) + 1) % 5);
-					_m[tile].m4 &= ~0x07;
-					_m[tile].m4 |= type ;
+					SB(_m[tile].m4, 0, 3, type);
 				} else {
 					// cycle between two-way -> one-way -> one-way -> ...
 					/* TODO: Rewrite switch into something more general */
@@ -961,9 +955,9 @@ int32 CmdRemoveSingleSignal(int x, int y, uint32 flags, uint32 p1, uint32 p2)
 		_m[tile].m3 &= ~SignalOnTrack(track);
 
 		/* removed last signal from tile? */
-		if ((_m[tile].m3 & 0xF0) == 0) {
-			_m[tile].m5 &= ~RAIL_TYPE_SIGNALS;
-			_m[tile].m2 &= ~0xF0;
+		if (GB(_m[tile].m3, 4, 4) == 0) {
+			SB(_m[tile].m2, 4, 4, 0);
+			SB(_m[tile].m5, 6, 2, RAIL_TYPE_NORMAL >> 6); // XXX >> because the constant is meant for direct application, not use with SB
 			CLRBIT(_m[tile].m4, 3); // remove any possible semaphores
 		}
 
@@ -997,7 +991,7 @@ static int32 DoConvertRail(TileIndex tile, uint totype, bool exec)
 
 	// change type.
 	if (exec) {
-		_m[tile].m3 = (_m[tile].m3 & 0xF0) + totype;
+		SB(_m[tile].m3, 4, 4, totype);
 		MarkTileDirtyByTile(tile);
 	}
 
@@ -1757,10 +1751,10 @@ bool SignalVehicleCheck(TileIndex tile, uint track)
 	 * is some kind of invisible black hole, and there is some special magic going
 	 * on in there. This 'workaround' can be removed once the maprewrite is done.
 	 */
-	if (GetTileType(tile)==MP_TUNNELBRIDGE && ((_m[tile].m5 & 0xF0)==0)) {
+	if (GetTileType(tile) == MP_TUNNELBRIDGE && GB(_m[tile].m5, 4, 4) == 0) {
 		// It is a tunnel we're checking, we need to do some special stuff
 		// because VehicleFromPos will not find the vihicle otherwise
-		byte direction = _m[tile].m5 & 3;
+		byte direction = GB(_m[tile].m5, 0, 2);
 		FindLengthOfTunnelResult flotr;
 		flotr = FindLengthOfTunnel(tile, direction);
 		dest.track = 1 << (direction & 1); // get the trackbit the vehicle would have if it has not entered the tunnel yet (ie is still visible)
@@ -1845,8 +1839,7 @@ static void ChangeSignalStates(SetSignalsData *ssd)
 	if (_patches.auto_pbs_placement && !(ssd->stop) && (ssd->has_pbssignal == 0xE) && !ssd->has_presignal && (ssd->presignal_exits == 0)) // 0xE means at least 2 pbs signals, and at least 1 entry and 1 exit, see comments ssd->has_pbssignal
 	for(i=0; i!=ssd->pbs_cur; i++) {
 		TileIndex tile = ssd->pbs_tile[i];
-		_m[tile].m4 &= ~0x07;
-		_m[tile].m4 |= 0x04;
+		SB(_m[tile].m4, 0, 3, SIGTYPE_PBS);
 		MarkTileDirtyByTile(tile);
 	};
 
@@ -2006,7 +1999,7 @@ static void TileLoop_Track(TileIndex tile)
 	uint16 m2;
 	byte owner;
 
-	m2 = _m[tile].m2 & 0xF;
+	m2 = GB(_m[tile].m2, 0, 4);
 
 	/* special code for alps landscape */
 	if (_opt.landscape == LT_HILLY) {
@@ -2158,7 +2151,7 @@ static void GetTileDesc_Track(TileIndex tile, TileDesc *td)
 				STR_NULL, STR_NULL
 			};
 
-			td->str = signal_type[_m[tile].m4 & 0x7];
+			td->str = signal_type[GB(_m[tile].m4, 0, 3)];
 			break;
 		}
 

depot.h
@@ -104,10 +104,10 @@ static inline DiagDirection GetDepotDirection(TileIndex tile, TransportType type
 		case TRANSPORT_RAIL:
 		case TRANSPORT_ROAD:
 			/* Rail and road store a diagonal direction in bits 0 and 1 */
-			return (DiagDirection)(_m[tile].m5 & 3);
+			return (DiagDirection)GB(_m[tile].m5, 0, 2);
 		case TRANSPORT_WATER:
 			/* Water is stubborn, it stores the directions in a different order. */
-			switch (_m[tile].m5 & 3) {
+			switch (GB(_m[tile].m5, 0, 2)) {
 				case 0: return DIAGDIR_NE;
 				case 1: return DIAGDIR_SW;
 				case 2: return DIAGDIR_NW;

industry_cmd.c
@@ -351,7 +351,7 @@ static void DrawTile_Industry(TileInfo *ti)
 	ormod = (ind->color_map+0x307) << PALETTE_SPRITE_START;
 
 	/* Retrieve pointer to the draw industry tile struct */
-	dits = &_industry_draw_tile_data[(ti->map5 << 2) | (_m[ti->tile].m1 & 3)];
+	dits = &_industry_draw_tile_data[(ti->map5 << 2) | GB(_m[ti->tile].m1, 0, 2)];
 
 	image = dits->sprite_1;
 	if (image & PALETTE_MODIFIER_COLOR && (image & PALETTE_SPRITE_MASK) == 0)

macros.h
@@ -145,11 +145,11 @@ static inline void swap_tile(TileIndex *a, TileIndex *b) { TileIndex t = *a; *a 
 #endif
 
 /// Fetch n bits starting at bit s from x
-#define GB(x, s, n) (((x) >> (s)) & ((1 << (n)) - 1))
+#define GB(x, s, n) (((x) >> (s)) & ((1U << (n)) - 1))
 /// Set n bits starting at bit s in x to d
-#define SB(x, s, n, d) ((x) = ((x) & ~(((1 << (n)) - 1) << (s))) | ((d) << (s)))
+#define SB(x, s, n, d) ((x) = ((x) & ~(((1U << (n)) - 1) << (s))) | ((d) << (s)))
 /// Add i to the n bits starting at bit s in x
-#define AB(x, s, n, i) ((x) = ((x) & ~(((1 << (n)) - 1) << (s))) | (((x) + ((i) << (s))) & (((1 << (n)) - 1) << (s))))
+#define AB(x, s, n, i) ((x) = ((x) & ~(((1U << (n)) - 1) << (s))) | (((x) + ((i) << (s))) & (((1U << (n)) - 1) << (s))))
 
 /**
  * ROtate x Left/Right by n (must be >= 0)

npf.c
@@ -60,8 +60,10 @@ static bool IsEndOfLine(TileIndex tile, Trackdir trackdir, RailType enginetype)
 	uint32 ts;
 
 	/* Can always go into a tunnel */
-	if (IsTileType(tile, MP_TUNNELBRIDGE) && (_m[tile].m5 & 0xF0)==0 && (_m[tile].m5 & 3) == exitdir)
+	if (IsTileType(tile, MP_TUNNELBRIDGE) && GB(_m[tile].m5, 4, 4) == 0 &&
+			GB(_m[tile].m5, 0, 2) == exitdir) {
 		return false;
+	}
 
 	/* Cannot go through the back of a depot */
 	if (IsTileDepotType(tile, TRANSPORT_RAIL) && (exitdir != GetDepotDirection(tile, TRANSPORT_RAIL)))
@@ -88,8 +90,11 @@ static bool IsEndOfLine(TileIndex tile, Trackdir trackdir, RailType enginetype)
 			return true;
 
 		/* Prevent us from falling off a slope into a tunnel exit */
-		if (IsTileType(dst_tile, MP_TUNNELBRIDGE) && (_m[dst_tile].m5 & 0xF0)==0 && (DiagDirection)(_m[dst_tile].m5 & 3) == ReverseDiagdir(exitdir))
-				return true;
+		if (IsTileType(dst_tile, MP_TUNNELBRIDGE) &&
+				GB(_m[dst_tile].m5, 4, 4) == 0 &&
+				(DiagDirection)GB(_m[dst_tile].m5, 0, 2) == ReverseDiagdir(exitdir)) {
+			return true;
+		}
 
 		/* Check for oneway signal against us */
 		if (IsTileType(dst_tile, MP_RAILWAY) && GetRailTileType(dst_tile) == RAIL_TYPE_SIGNALS) {
@@ -312,7 +317,7 @@ static uint NPFTunnelCost(AyStarNode* current)
 {
 	DiagDirection exitdir = TrackdirToExitdir((Trackdir)current->direction);
 	TileIndex tile = current->tile;
-	if ( (DiagDirection)(_m[tile].m5 & 3) == ReverseDiagdir(exitdir)) {
+	if ((DiagDirection)GB(_m[tile].m5, 0, 2) == ReverseDiagdir(exitdir)) {
 		/* We just popped out if this tunnel, since were
 		 * facing the tunnel exit */
 		FindLengthOfTunnelResult flotr;
@@ -364,13 +369,13 @@ static void NPFMarkTile(TileIndex tile)
 				/* DEBUG: mark visited tiles by mowing the grass under them
 				 * ;-) */
 				if (!IsTileDepotType(tile, TRANSPORT_RAIL)) {
-					_m[tile].m2 &= ~15; /* Clear bits 0-3 */
+					SB(_m[tile].m2, 0, 4, 0);
 					MarkTileDirtyByTile(tile);
 				}
 				break;
 			case MP_STREET:
 				if (!IsTileDepotType(tile, TRANSPORT_ROAD)) {
-					_m[tile].m4 &= ~0x70; /* Clear bits 4-6 */
+					SB(_m[tile].m2, 4, 3, 0);
 					MarkTileDirtyByTile(tile);
 				}
 				break;
@@ -408,7 +413,7 @@ static int32 NPFRoadPathCost(AyStar* as, AyStarNode* current, OpenListNode* pare
 	/* Determine base length */
 	switch (GetTileType(tile)) {
 		case MP_TUNNELBRIDGE:
-			if ((_m[tile].m5 & 0xF0)==0) {
+			if (GB(_m[tile].m5, 4, 4) == 0) {
 				cost = NPFTunnelCost(current);
 				break;
 			}
@@ -452,7 +457,7 @@ static int32 NPFRailPathCost(AyStar* as, AyStarNode* current, OpenListNode* pare
 	/* Determine base length */
 	switch (GetTileType(tile)) {
 		case MP_TUNNELBRIDGE:
-			if ((_m[tile].m5 & 0xF0)==0) {
+			if (GB(_m[tile].m5, 4, 4) == 0) {
 				cost = NPFTunnelCost(current);
 				break;
 			}
@@ -659,10 +664,10 @@ static bool VehicleMayEnterTile(Owner owner, TileIndex tile, DiagDirection enter
  * intensive owner check, instead we will just assume that if the vehicle
  * managed to get on the bridge, it is probably allowed to :-)
  */
-			if ((_m[tile].m5 & 0xC6) == 0xC0 && (unsigned)(_m[tile].m5 & 0x1) == (enterdir & 0x1)) {
+			if ((_m[tile].m5 & 0xC6) == 0xC0 && GB(_m[tile].m5, 0, 1) == (enterdir & 0x1)) {
 				/* on the middle part of a railway bridge: find bridge ending */
 				while (IsTileType(tile, MP_TUNNELBRIDGE) && !((_m[tile].m5 & 0xC6) == 0x80)) {
-					tile += TileOffsByDir(_m[tile].m5 & 0x1);
+					tile += TileOffsByDir(GB(_m[tile].m5, 0, 1));
 				}
 			}
 			/* if we were on a railway middle part, we are now at a railway bridge ending */
@@ -670,7 +675,7 @@ static bool VehicleMayEnterTile(Owner owner, TileIndex tile, DiagDirection enter
 			if (
 				(_m[tile].m5 & 0xFC) == 0 /* railway tunnel */
 				|| (_m[tile].m5 & 0xC6) == 0x80 /* railway bridge ending */
-				|| ((_m[tile].m5 & 0xF8) == 0xE0 && ((unsigned)_m[tile].m5 & 0x1) != (enterdir & 0x1)) /* railway under bridge */
+				|| ((_m[tile].m5 & 0xF8) == 0xE0 && GB(_m[tile].m5, 0, 1) != (enterdir & 0x1)) /* railway under bridge */
 				)
 				return IsTileOwner(tile, owner);
 			break;
@@ -704,7 +709,8 @@ static void NPFFollowTrack(AyStar* aystar, OpenListNode* current)
 	aystar->EndNodeCheck(aystar, current);
 
 	/* Find dest tile */
-	if (IsTileType(src_tile, MP_TUNNELBRIDGE) && (_m[src_tile].m5 & 0xF0)==0 && (DiagDirection)(_m[src_tile].m5 & 3) == src_exitdir) {
+	if (IsTileType(src_tile, MP_TUNNELBRIDGE) && GB(_m[src_tile].m5, 4, 4) == 0 &&
+			(DiagDirection)GB(_m[src_tile].m5, 0, 2) == src_exitdir) {
 		/* This is a tunnel. We know this tunnel is our type,
 		 * otherwise we wouldn't have got here. It is also facing us,
 		 * so we should skip it's body */
@@ -749,8 +755,10 @@ static void NPFFollowTrack(AyStar* aystar, OpenListNode* current)
 	/* I can't enter a tunnel entry/exit tile from a tile above the tunnel. Note
 	 * that I can enter the tunnel from a tile below the tunnel entrance. This
 	 * solves the problem of vehicles wanting to drive off a tunnel entrance */
-	if (IsTileType(dst_tile, MP_TUNNELBRIDGE) && (_m[dst_tile].m5 & 0xF0) == 0 && GetTileZ(dst_tile) < GetTileZ(src_tile))
+	if (IsTileType(dst_tile, MP_TUNNELBRIDGE) && GB(_m[dst_tile].m5, 4, 4) == 0 &&
+			GetTileZ(dst_tile) < GetTileZ(src_tile)) {
 		return;
+	}
 
 	/* check correct rail type (mono, maglev, etc) */
 	if (type == TRANSPORT_RAIL) {

pathfind.c
@@ -209,7 +209,7 @@ static const int8 _get_tunlen_inc[5] = { -16, 0, 16, 0, -16 };
 /* Returns the end tile and the length of a tunnel. The length does not
  * include the starting tile (entry), it does include the end tile (exit).
  */
-FindLengthOfTunnelResult FindLengthOfTunnel(TileIndex tile, int direction)
+FindLengthOfTunnelResult FindLengthOfTunnel(TileIndex tile, uint direction)
 {
 	FindLengthOfTunnelResult flotr;
 	int x,y;
@@ -231,11 +231,12 @@ FindLengthOfTunnelResult FindLengthOfTunnel(TileIndex tile, int direction)
 		tile = TileVirtXY(x, y);
 
 		if (IsTileType(tile, MP_TUNNELBRIDGE) &&
-				(_m[tile].m5 & 0xF0) == 0 &&					// tunnel entrance/exit
-				//((_m[tile].m5>>2)&3) == type &&		// rail/road-tunnel <-- This is not necesary to check, right?
-				((_m[tile].m5 & 3)^2) == direction &&	// entrance towards: 0 = NE, 1 = SE, 2 = SW, 3 = NW
-				GetSlopeZ(x+8, y+8) == z)
-					break;
+				GB(_m[tile].m5, 4, 4) == 0 &&               // tunnel entrance/exit
+				// GB(_m[tile].m5, 2, 2) == type &&            // rail/road-tunnel <-- This is not necesary to check, right?
+				(GB(_m[tile].m5, 0, 2) ^ 2) == direction && // entrance towards: 0 = NE, 1 = SE, 2 = SW, 3 = NW
+				GetSlopeZ(x + 8, y + 8) == z) {
+			break;
+		}
 	}
 
 	flotr.tile = tile;
@@ -274,16 +275,18 @@ const byte _ffb_64[128] = {
 48,56,56,58,56,60,60,62,
 };
 
-static void TPFMode1(TrackPathFinder *tpf, TileIndex tile, int direction)
+static void TPFMode1(TrackPathFinder *tpf, TileIndex tile, uint direction)
 {
 	uint bits;
 	int i;
 	RememberData rd;
 	TileIndex tile_org = tile;
 
-	if (IsTileType(tile, MP_TUNNELBRIDGE) && (_m[tile].m5 & 0xF0) == 0) {
-		if ((_m[tile].m5 & 3) != direction || ((_m[tile].m5>>2)&3) != tpf->tracktype)
+	if (IsTileType(tile, MP_TUNNELBRIDGE) && GB(_m[tile].m5, 4, 4) == 0) {
+		if (GB(_m[tile].m5, 0, 2) != direction ||
+				GB(_m[tile].m5, 2, 2) != tpf->tracktype) {
 			return;
+		}
 		tile = SkipToEndOfTunnel(tpf, tile, direction);
 	}
 	tile += TileOffsByDir(direction);
@@ -704,11 +707,11 @@ start_at:
 		// If the tile is the entry tile of a tunnel, and we're not going out of the tunnel,
 		//   need to find the exit of the tunnel.
 		if (IsTileType(tile, MP_TUNNELBRIDGE)) {
-			if ((_m[tile].m5 & 0xF0) == 0 &&
-				(uint)(_m[tile].m5 & 3) != (direction ^ 2)) {
+			if (GB(_m[tile].m5, 4, 4) == 0 &&
+					GB(_m[tile].m5, 0, 2) != (direction ^ 2)) {
 				/* This is a tunnel tile */
 				/* We are not just driving out of the tunnel */
-				if ( (uint)(_m[tile].m5 & 3) != direction || GB(_m[tile].m5, 2, 2) != tpf->tracktype)
+				if (GB(_m[tile].m5, 0, 2) != direction || GB(_m[tile].m5, 2, 2) != tpf->tracktype)
 					/* We are not driving into the tunnel, or it
 					 * is an invalid tunnel */
 					continue;
@@ -761,7 +764,7 @@ start_at:
 
 			// The tile has no reachable tracks, or
 			// does the tile contain more than one track?
-			if (bits == 0 || KILL_FIRST_BIT(_m[tile].m5 & 0x3F) != 0)
+			if (bits == 0 || KILL_FIRST_BIT(GB(_m[tile].m5, 0, 6)) != 0)
 				break;
 
 			// If we reach here, the tile has exactly one track, and this

pathfind.h
@@ -64,7 +64,7 @@ typedef struct {
 	TileIndex tile;
 	int length;
 } FindLengthOfTunnelResult;
-FindLengthOfTunnelResult FindLengthOfTunnel(TileIndex tile, int direction);
+FindLengthOfTunnelResult FindLengthOfTunnel(TileIndex tile, uint direction);
 
 void NewTrainPathfind(TileIndex tile, TileIndex dest, byte direction, NTPEnumProc *enum_proc, void *data);
 

pbs.c
@@ -57,15 +57,14 @@ void PBSReserveTrack(TileIndex tile, Track track) {
 				SETBIT(_m[tile].m3, 6);
 			} else {
 				// normal rail track
-				byte encrt = (_m[tile].m4 & 0xF0) >> 4; // get current encoded info (see comments at top of file)
+				byte encrt = GB(_m[tile].m4, 4, 4); // get current encoded info (see comments at top of file)
 
 				if (encrt == 0) // nothing reserved before
 					encrt = track + 1;
 				else if (encrt == (track^1) + 1) // opposite track reserved before
 					encrt |= 8;
 
-				_m[tile].m4 &= ~0xF0;
-				_m[tile].m4 |= encrt << 4;
+				SB(_m[tile].m4, 4, 4, encrt);
 			}
 			break;
 		case MP_TUNNELBRIDGE:
@@ -99,12 +98,12 @@ byte PBSTileReserved(TileIndex tile) {
 				return HASBIT(_m[tile].m5, 0) ? 2 : 1;
 			} else {
 				// normal track
-				byte res = encrt_to_reserved[(_m[tile].m4 & 0xF0) >> 4];
+				byte res = encrt_to_reserved[GB(_m[tile].m4, 4, 4)];
 				assert(res != 0xFF);
 				return res;
 			}
 		case MP_TUNNELBRIDGE:
-			return (_m[tile].m4 & 3);
+			return GB(_m[tile].m4, 0, 2);
 		case MP_STATION:
 			// check if its reserved
 			if (!HASBIT(_m[tile].m3, 6)) return 0;
@@ -131,12 +130,12 @@ uint16 PBSTileUnavail(TileIndex tile) {
 				return HASBIT(_m[tile].m3, 6) ? TRACKDIR_BIT_MASK : 0;
 			} else {
 				// normal track
-				uint16 res = encrt_to_unavail[(_m[tile].m4 & 0xF0) >> 4];
+				uint16 res = encrt_to_unavail[GB(_m[tile].m4, 4, 4)];
 				assert(res != 0xFFFF);
 				return res;
 			}
 		case MP_TUNNELBRIDGE:
-			return (_m[tile].m4 & 3) | ((_m[tile].m4 & 3) << 8);
+			return GB(_m[tile].m4, 0, 2) | (GB(_m[tile].m4, 0, 2) << 8);
 		case MP_STATION:
 			return HASBIT(_m[tile].m3, 6) ? TRACKDIR_BIT_MASK : 0;
 		case MP_STREET:
@@ -159,7 +158,7 @@ void PBSClearTrack(TileIndex tile, Track track) {
 				CLRBIT(_m[tile].m3, 6);
 			} else {
 				// normal rail track
-				byte encrt = (_m[tile].m4 & 0xF0) >> 4;
+				byte encrt = GB(_m[tile].m4, 4, 4);
 
 				if (encrt == track + 1)
 					encrt = 0;
@@ -168,8 +167,7 @@ void PBSClearTrack(TileIndex tile, Track track) {
 				else if (encrt == (track^1) + 1 + 8)
 					encrt &= 7;
 
-				_m[tile].m4 &= ~0xF0;
-				_m[tile].m4 |= encrt << 4;
+				SB(_m[tile].m4, 4, 4, encrt);
 			}
 			break;
 		case MP_TUNNELBRIDGE:
@@ -203,7 +201,9 @@ void PBSClearPath(TileIndex tile, Trackdir trackdir, TileIndex end_tile, Trackdi
 		if (tile == end_tile && TrackdirToTrack(trackdir) == TrackdirToTrack(end_trackdir))
 			return;
 
-		if (IsTileType(tile, MP_TUNNELBRIDGE) && (_m[tile].m5 & 0xF0)==0 && (unsigned)(_m[tile].m5 & 3) == TrackdirToExitdir(trackdir)) {
+		if (IsTileType(tile, MP_TUNNELBRIDGE) &&
+				GB(_m[tile].m5, 4, 4) == 0 &&
+				GB(_m[tile].m5, 0, 2) == TrackdirToExitdir(trackdir)) {
 			// this is a tunnel
 			flotr = FindLengthOfTunnel(tile, TrackdirToExitdir(trackdir));
 

commit 90f80da14e8b64d12aa95ce6bc310d4c5cc555c1

-Fix: Rail convert issue introduced in r3019
git-svn-id: http://svn.openttd.org/trunk@3025 6aa0318a-3be1-0310-93fa-89fd2396df07

rail_cmd.c
@@ -991,7 +991,7 @@ static int32 DoConvertRail(TileIndex tile, uint totype, bool exec)
 
 	// change type.
 	if (exec) {
-		SB(_m[tile].m3, 4, 4, totype);
+		SB(_m[tile].m3, 0, 4, totype);
 		MarkTileDirtyByTile(tile);
 	}
 