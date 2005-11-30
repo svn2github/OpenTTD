svn2github/OpenTTD

commit 4aa78f692cb834374c1b2f3cdcfed5cd50f8c422

- Fix: [ 1327011 ] Fix: don't lower land on tunnel, even with diag tracks on it (based on patch by glx)
git-svn-id: http://svn.openttd.org/trunk@3228 6aa0318a-3be1-0310-93fa-89fd2396df07

clear_cmd.c
@@ -86,7 +86,7 @@ static void TerraformAddDirtyTileAround(TerraformerState *ts, TileIndex tile)
 static int TerraformProc(TerraformerState *ts, TileIndex tile, int mode)
 {
 	int r;
-	int32 ret;
+	bool skip_clear = false;
 
 	assert(tile < MapSize());
 
@@ -111,17 +111,19 @@ static int TerraformProc(TerraformerState *ts, TileIndex tile, int mode)
 
 		// If we have a single diagonal track there, the other side of
 		// tile can be terraformed.
-		if ((_m[tile].m5 & ~0x40) == _railway_modes[mode]) return 0;
+		if ((_m[tile].m5 & ~0x40) == _railway_modes[mode]) skip_clear = true;
 	}
 
-	ret = DoCommandByTile(tile, 0,0, ts->flags & ~DC_EXEC, CMD_LANDSCAPE_CLEAR);
+	if (!skip_clear) {
+		int32 ret = DoCommandByTile(tile, 0,0, ts->flags & ~DC_EXEC, CMD_LANDSCAPE_CLEAR);
 
-	if (ret == CMD_ERROR) {
-		_terraform_err_tile = tile;
-		return -1;
+		if (CmdFailed(ret)) {
+			_terraform_err_tile = tile;
+			return -1;
+		}
+
+		ts->cost += ret;
 	}
-
-	ts->cost += ret;
 
 	if (ts->tile_table_count >= 625) return -1;
 	ts->tile_table[ts->tile_table_count++] = tile;

commit ffd475defe8e9ea37370c40333cd4882114ffb92

Fix for issue introduced in r3228. The original code was good for raising, then for lowering, now for both.
git-svn-id: http://svn.openttd.org/trunk@3249 6aa0318a-3be1-0310-93fa-89fd2396df07

clear_cmd.c
@@ -111,7 +111,10 @@ static int TerraformProc(TerraformerState *ts, TileIndex tile, int mode)
 
 		// If we have a single diagonal track there, the other side of
 		// tile can be terraformed.
-		if ((_m[tile].m5 & ~0x40) == _railway_modes[mode]) skip_clear = true;
+		if ((_m[tile].m5 & ~0x40) == _railway_modes[mode]) {
+			if (ts->direction == 1) return 0;
+			skip_clear = true;
+		}
 	}
 
 	if (!skip_clear) {