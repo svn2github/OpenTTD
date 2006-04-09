svn2github/OpenTTD

commit 50be2d6a3ab491df3305ae9d1cfa9d34fa0d3d05

-Fix: fixed power computation for mixed consists on level crossings (michi_cc)
git-svn-id: http://svn.openttd.org/trunk@4290 6aa0318a-3be1-0310-93fa-89fd2396df07

train_cmd.c
@@ -92,6 +92,9 @@ void TrainPowerChanged(Vehicle* v)
 		if (IsBridgeTile(u->tile) && IsBridgeMiddle(u->tile) && DiagDirToAxis(DirToDiagDir(u->direction)) == GetBridgeAxis(u->tile)) {
 			if (!HasPowerOnRail(u->u.rail.railtype, GetRailTypeOnBridge(u->tile))) engine_has_power = false;
 			if (!HasPowerOnRail(v->u.rail.railtype, GetRailTypeOnBridge(u->tile))) wagon_has_power = false;
+		} else if (IsLevelCrossing(u->tile)) {
+			if (!HasPowerOnRail(u->u.rail.railtype, GetRailTypeCrossing(u->tile)))	engine_has_power = false;
+			if (!HasPowerOnRail(v->u.rail.railtype, GetRailTypeCrossing(u->tile)))	wagon_has_power = false;
 		} else {
 			if (!HasPowerOnRail(u->u.rail.railtype, GetRailType(u->tile))) engine_has_power = false;
 			if (!HasPowerOnRail(v->u.rail.railtype, GetRailType(u->tile))) wagon_has_power = false;

commit a4d0b30f883a0b75e28f08460d5b7921569f0c66

-Fix (FS#105): Bug introduced in r4290. IsLevelCrossing does NOT check the tile type, it assumes the tile type IS MP_STREET
git-svn-id: http://svn.openttd.org/trunk@4338 6aa0318a-3be1-0310-93fa-89fd2396df07

train_cmd.c
@@ -92,7 +92,7 @@ void TrainPowerChanged(Vehicle* v)
 		if (IsBridgeTile(u->tile) && IsBridgeMiddle(u->tile) && DiagDirToAxis(DirToDiagDir(u->direction)) == GetBridgeAxis(u->tile)) {
 			if (!HasPowerOnRail(u->u.rail.railtype, GetRailTypeOnBridge(u->tile))) engine_has_power = false;
 			if (!HasPowerOnRail(v->u.rail.railtype, GetRailTypeOnBridge(u->tile))) wagon_has_power = false;
-		} else if (IsLevelCrossing(u->tile)) {
+		} else if (IsTileType(u->tile, MP_STREET) && IsLevelCrossing(u->tile)) {
 			if (!HasPowerOnRail(u->u.rail.railtype, GetRailTypeCrossing(u->tile)))	engine_has_power = false;
 			if (!HasPowerOnRail(v->u.rail.railtype, GetRailTypeCrossing(u->tile)))	wagon_has_power = false;
 		} else {