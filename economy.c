svn2github/OpenTTD

commit 631dbeaa4cea8e016b719656013504cf17a2c3f7

Several smaller changes: - Don't treat non-booleans as booleans - Reduce variable scope - Bracing - Use DeMorgan's law to make conditionals easier to read - if cascade -> switch - Replace some magic numbers by symbolic names - Avoid assignments within other statements
git-svn-id: http://svn.openttd.org/trunk@3564 6aa0318a-3be1-0310-93fa-89fd2396df07

economy.c
@@ -346,14 +346,12 @@ void ChangeOwnershipOfPlayerItems(PlayerID old_player, PlayerID new_player)
 					DeleteVehicle(v);
 				} else {
 					v->owner = new_player;
-					if (v->type == VEH_Train && IsFrontEngine(v))
-						v->unitnumber = ++num_train;
-					else if (v->type == VEH_Road)
-						v->unitnumber = ++num_road;
-					else if (v->type == VEH_Ship)
-						v->unitnumber = ++num_ship;
-					else if (v->type == VEH_Aircraft && v->subtype <= 2)
-						v->unitnumber = ++num_aircraft;
+					switch (v->type) {
+						case VEH_Train:    if (IsFrontEngine(v)) v->unitnumber = ++num_train; break;
+						case VEH_Road:     v->unitnumber = ++num_road; break;
+						case VEH_Ship:     v->unitnumber = ++num_ship; break;
+						case VEH_Aircraft: if (v->subtype <= 2) v->unitnumber = ++num_aircraft; break;
+					}
 				}
 			}
 		}
@@ -1190,10 +1188,17 @@ static bool CheckSubsidised(Station *from, Station *to, byte cargo_type)
 			if (DistanceMax(xy, from->xy) > 9) continue;
 
 			/* Check distance from dest */
-			if (cargo_type == CT_PASSENGERS || cargo_type == CT_MAIL || cargo_type == CT_GOODS || cargo_type == CT_FOOD) {
-				xy = GetTown(s->to)->xy;
-			} else {
-				xy = (GetIndustry(s->to))->xy;
+			switch (cargo_type) {
+				case CT_PASSENGERS:
+				case CT_MAIL:
+				case CT_GOODS:
+				case CT_FOOD:
+					xy = GetTown(s->to)->xy;
+					break;
+
+				default:
+					xy = GetIndustry(s->to)->xy;
+					break;
 			}
 			if (DistanceMax(xy, to->xy) > 9) continue;
 
@@ -1256,18 +1261,11 @@ static int32 DeliverGoods(int num_pieces, byte cargo_type, uint16 source, uint16
 
 	// Modify profit if a subsidy is in effect
 	if (subsidised) {
-		if (_opt.diff.subsidy_multiplier < 1) {
-			/* 1.5x */
-			profit += profit >> 1;
-		} else if (_opt.diff.subsidy_multiplier == 1) {
-			/* 2x */
-			profit *= 2;
-		} else if (_opt.diff.subsidy_multiplier == 2) {
-			/* 3x */
-			profit *= 3;
-		} else {
-			/* 4x */
-			profit *= 4;
+		switch (_opt.diff.subsidy_multiplier) {
+			case 0:  profit += profit >> 1;
+			case 1:  profit *= 2;
+			case 2:  profit *= 3;
+			default: profit *= 4;
 		}
 	}
 
@@ -1353,7 +1351,8 @@ int LoadUnloadVehicle(Vehicle *v)
 	old_player = _current_player;
 	_current_player = v->owner;
 
-	st = GetStation(last_visited = v->last_station_visited);
+	last_visited = v->last_station_visited;
+	st = GetStation(last_visited);
 
 	for (; v != NULL; v = v->next) {
 		GoodsEntry* ge;
@@ -1413,12 +1412,14 @@ int LoadUnloadVehicle(Vehicle *v)
 
 		/* update stats */
 		ge->days_since_pickup = 0;
-		t = u->max_speed;
-		if (u->type == VEH_Road) t >>=1;
-		if (u->type == VEH_Train) t = u->u.rail.cached_max_speed;
+		switch (u->type) {
+			case VEH_Train: t = u->u.rail.cached_max_speed; break;
+			case VEH_Road:  t = u->max_speed / 2;           break;
+			default:        t = u->max_speed;               break;
+		}
 
 		// if last speed is 0, we treat that as if no vehicle has ever visited the station.
-		ge->last_speed = t < 255 ? t : 255;
+		ge->last_speed = min(t, 255);
 		ge->last_age = _cur_year - v->build_year;
 
 		// If there's goods waiting at the station, and the vehicle

ai/default/default.c
@@ -2778,8 +2778,7 @@ static bool AiEnumFollowRoad(TileIndex tile, AiRoadEnum *a, int track, uint leng
 	if (dist <= a->best_dist) {
 		TileIndex tile2 = TILE_MASK(tile + TileOffsByDir(_dir_by_track[track]));
 
-		if (IsTileType(tile2, MP_STREET) &&
-				(_m[tile2].m5&0xF0) == 0) {
+		if (IsTileType(tile2, MP_STREET) && GB(_m[tile2].m5, 4, 4) == 0) {
 			a->best_dist = dist;
 			a->best_tile = tile;
 			a->best_track = track;

aircraft_cmd.c
@@ -98,7 +98,7 @@ int GetAircraftImage(const Vehicle *v, byte direction)
 	if (is_custom_sprite(spritenum)) {
 		int sprite = GetCustomVehicleSprite(v, direction);
 
-		if (sprite) return sprite;
+		if (sprite != 0) return sprite;
 		spritenum = orig_aircraft_vehicle_info[v->engine_type - AIRCRAFT_ENGINES_INDEX].image_index;
 	}
 	return direction + _aircraft_sprite[spritenum];

clear_cmd.c
@@ -637,10 +637,9 @@ static void TileLoop_Clear(TileIndex tile)
 {
 	TileLoopClearHelper(tile);
 
-	if (_opt.landscape == LT_DESERT) {
-		TileLoopClearDesert(tile);
-	} else if (_opt.landscape == LT_HILLY) {
-		TileLoopClearAlps(tile);
+	switch (_opt.landscape) {
+		case LT_DESERT: TileLoopClearDesert(tile); break;
+		case LT_HILLY:  TileLoopClearAlps(tile);   break;
 	}
 
 	switch (GetClearGround(tile)) {

disaster_cmd.c
@@ -898,10 +898,11 @@ static void Disaster6_Init(void)
 static void Disaster7_Init(void)
 {
 	int index = GB(Random(), 0, 4);
-	Industry *i;
 	uint m;
 
 	for (m = 0; m < 15; m++) {
+		const Industry* i;
+
 		FOR_ALL_INDUSTRIES(i) {
 			if (i->xy != 0 && i->type == IT_COAL_MINE && --index < 0) {
 				SetDParam(0, i->town->index);

industry_gui.c
@@ -280,22 +280,22 @@ static void IndustryViewWndProc(Window *w, WindowEvent *e)
 
 	switch (e->event) {
 	case WE_PAINT: {
-		const Industry *i;
-		StringID str;
+		const Industry* i = GetIndustry(w->window_number);
 
-		i = GetIndustry(w->window_number);
 		SetDParam(0, w->window_number);
 		DrawWindowWidgets(w);
 
 		if (i->accepts_cargo[0] != CT_INVALID) {
+			StringID str;
+
 			SetDParam(0, _cargoc.names_s[i->accepts_cargo[0]]);
 			str = STR_4827_REQUIRES;
 			if (i->accepts_cargo[1] != CT_INVALID) {
 				SetDParam(1, _cargoc.names_s[i->accepts_cargo[1]]);
-				str++;
+				str = STR_4828_REQUIRES;
 				if (i->accepts_cargo[2] != CT_INVALID) {
 					SetDParam(2, _cargoc.names_s[i->accepts_cargo[2]]);
-					str++;
+					str = STR_4829_REQUIRES;
 				}
 			}
 			DrawString(2, 107, str, 0);
@@ -442,17 +442,14 @@ static const WindowDesc _industry_view_desc = {
 
 void ShowIndustryViewWindow(int industry)
 {
-	Window *w;
-	Industry *i;
+	Window* w = AllocateWindowDescFront(&_industry_view_desc, industry);
 
-	w = AllocateWindowDescFront(&_industry_view_desc, industry);
-	if (w) {
+	if (w != NULL) {
 		w->flags4 |= WF_DISABLE_VP_SCROLL;
 		WP(w,vp2_d).data_1 = 0;
 		WP(w,vp2_d).data_2 = 0;
 		WP(w,vp2_d).data_3 = 0;
-		i = GetIndustry(w->window_number);
-		AssignWindowViewport(w, 3, 17, 0xFE, 0x56, i->xy + TileDiffXY(1, 1), 1);
+		AssignWindowViewport(w, 3, 17, 0xFE, 0x56, GetIndustry(w->window_number)->xy + TileDiffXY(1, 1), 1);
 	}
 }
 
@@ -550,8 +547,7 @@ static void MakeSortedIndustryList(void)
 		error("Could not allocate memory for the industry-sorting-list");
 
 	FOR_ALL_INDUSTRIES(i) {
-		if (i->xy)
-			_industry_sort[n++] = i->index;
+		if (i->xy != 0) _industry_sort[n++] = i->index;
 	}
 	_num_industry_sort = n;
 	_last_industry_idx = 0xFFFF; // used for "cache"
@@ -568,7 +564,6 @@ static void IndustryDirectoryWndProc(Window *w, WindowEvent *e)
 	case WE_PAINT: {
 		int n;
 		uint p;
-		Industry *i;
 		static const uint16 _indicator_positions[4] = {88, 187, 284, 387};
 
 		if (_industry_sort_dirty) {
@@ -585,7 +580,8 @@ static void IndustryDirectoryWndProc(Window *w, WindowEvent *e)
 		n = 0;
 
 		while (p < _num_industry_sort) {
-			i = GetIndustry(_industry_sort[p]);
+			const Industry* i = GetIndustry(_industry_sort[p]);
+
 			SetDParam(0, i->index);
 			if (i->produced_cargo[0] != CT_INVALID) {
 				SetDParam(1, _cargoc.names_long[i->produced_cargo[0]]);
@@ -638,14 +634,11 @@ static void IndustryDirectoryWndProc(Window *w, WindowEvent *e)
 		case 8: {
 			int y = (e->click.pt.y - 28) / 10;
 			uint16 p;
-			Industry *c;
 
-			if (!IS_INT_INSIDE(y, 0, w->vscroll.cap))
-				return;
+			if (!IS_INT_INSIDE(y, 0, w->vscroll.cap)) return;
 			p = y + w->vscroll.pos;
 			if (p < _num_industry_sort) {
-				c = GetIndustry(_industry_sort[p]);
-				ScrollMainWindowToTile(c->xy);
+				ScrollMainWindowToTile(GetIndustry(_industry_sort[p])->xy);
 			}
 		} break;
 		}
@@ -674,11 +667,9 @@ static const WindowDesc _industry_directory_desc = {
 
 void ShowIndustryDirectory(void)
 {
-	/* Industry List */
-	Window *w;
+	Window* w = AllocateWindowDescFront(&_industry_directory_desc, 0);
 
-	w = AllocateWindowDescFront(&_industry_directory_desc, 0);
-	if (w) {
+	if (w != NULL) {
 		w->vscroll.cap = 16;
 		w->resize.height = w->height - 6 * 10; // minimum 10 items
 		w->resize.step_height = 10;

misc.c
@@ -78,7 +78,9 @@ uint InteractiveRandomRange(uint max)
 void SetDate(uint date)
 {
 	YearMonthDay ymd;
-	ConvertDayToYMD(&ymd, _date = date);
+
+	_date = date;
+	ConvertDayToYMD(&ymd, date);
 	_cur_year = ymd.year;
 	_cur_month = ymd.month;
 #ifdef ENABLE_NETWORK
@@ -186,8 +188,6 @@ void InitializeGame(int mode, uint size_x, uint size_y)
 
 void GenerateWorld(int mode, uint size_x, uint size_y)
 {
-	int i;
-
 	// Make sure everything is done via OWNER_NONE
 	_current_player = OWNER_NONE;
 
@@ -223,7 +223,9 @@ void GenerateWorld(int mode, uint size_x, uint size_y)
 
 	// No need to run the tile loop in the scenario editor.
 	if (mode != GW_EMPTY) {
-		for (i = 0x500; i != 0; i--) RunTileLoop();
+		uint i;
+
+		for (i = 0; i < 0x500; i++) RunTileLoop();
 	}
 
 	ResetObjectToPlace();
@@ -410,7 +412,7 @@ typedef struct LandscapePredefVar {
 void InitializeLandscapeVariables(bool only_constants)
 {
 	const LandscapePredefVar *lpd;
-	int i;
+	uint i;
 	StringID str;
 
 	lpd = &_landscape_predef_var[_opt.landscape];
@@ -487,7 +489,8 @@ static const uint16 _autosave_months[] = {
  */
 static void RunVehicleDayProc(uint daytick)
 {
-	uint i, total = _vehicle_pool.total_items;
+	uint total = _vehicle_pool.total_items;
+	uint i;
 
 	for (i = daytick; i < total; i += DAY_TICKS) {
 		Vehicle* v = GetVehicle(i);
@@ -976,13 +979,13 @@ static void Save_CHTS(void)
 
 static void Load_CHTS(void)
 {
-	Cheat* cht = (Cheat*) &_cheats;
+	Cheat* cht = (Cheat*)&_cheats;
+	uint count = SlGetFieldLength() / 2;
+	uint i;
 
-	uint count = SlGetFieldLength()/2;
-	for (; count; count--, cht++)
-	{
-		cht->been_used = (byte)SlReadByte();
-		cht->value = (byte)SlReadByte();
+	for (i = 0; i < count; i++) {
+		cht[i].been_used = SlReadByte();
+		cht[i].value     = SlReadByte();
 	}
 }
 

network_data.c
@@ -100,8 +100,8 @@ void NetworkSend_Packet(Packet *packet, NetworkClientState *cs)
 	packet->pos = 0;
 	packet->next = NULL;
 
-	packet->buffer[0] = packet->size & 0xFF;
-	packet->buffer[1] = packet->size >> 8;
+	packet->buffer[0] = GB(packet->size, 0, 8);
+	packet->buffer[1] = GB(packet->size, 8, 8);
 
 	// Locate last packet buffered for the client
 	p = cs->packet_queue;

commit b569a8586d6f28f5c61dc2b3fc1f25ee4768ec90

- Fix: subsidy calculation was too high, broken in r3564
git-svn-id: http://svn.openttd.org/trunk@4570 6aa0318a-3be1-0310-93fa-89fd2396df07

economy.c
@@ -1240,10 +1240,10 @@ static int32 DeliverGoods(int num_pieces, CargoID cargo_type, StationID source, 
 	// Modify profit if a subsidy is in effect
 	if (subsidised) {
 		switch (_opt.diff.subsidy_multiplier) {
-			case 0:  profit += profit >> 1;
-			case 1:  profit *= 2;
-			case 2:  profit *= 3;
-			default: profit *= 4;
+			case 0:  profit += profit >> 1; break;
+			case 1:  profit *= 2; break;
+			case 2:  profit *= 3; break;
+			default: profit *= 4; break;
 		}
 	}
 