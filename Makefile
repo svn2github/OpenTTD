svn2github/OpenTTD

commit 645aa3444b21ac2b6ac5e60eb10c4dde66b6ee49

- [Merge] [GPMI] -Fix: finally OTTD doesn't compile if you do a 'make mrproper' (TrueLight)
git-svn-id: http://svn.openttd.org/trunk@3574 6aa0318a-3be1-0310-93fa-89fd2396df07

Makefile
@@ -972,7 +972,7 @@ depend:
 	@true # The include handles this automagically
 
 # Introduce the dependencies
-ifeq ($(findstring $(MAKECMDGOALS), clean info),)
+ifeq ($(findstring $(MAKECMDGOALS), clean info depend mrproper),)
 -include $(DEPS)
 endif
 

commit 1ab839f487eaf0d7ae7e22002290434a98369380

Fix 'make depend' which was broken in r3574 (it just did nothing)
git-svn-id: http://svn.openttd.org/trunk@5923 6aa0318a-3be1-0310-93fa-89fd2396df07

Makefile
@@ -1037,7 +1037,7 @@ depend:
 	@true # The include handles this automagically
 
 # Introduce the dependencies
-ifeq ($(findstring $(MAKECMDGOALS), clean info depend mrproper upgradeconf unittest $(MAKE_CONFIG)),)
+ifeq ($(findstring $(MAKECMDGOALS), clean info mrproper upgradeconf unittest $(MAKE_CONFIG)),)
 -include $(DEPS)
 endif
 

tunnel_map.c
@@ -4,6 +4,7 @@
 #include "openttd.h"
 #include "tile.h"
 #include "tunnel_map.h"
+#include "vehicle.h"
 
 TileIndex GetOtherTunnelEnd(TileIndex tile)
 {
@@ -19,6 +20,31 @@ TileIndex GetOtherTunnelEnd(TileIndex tile)
 		GetTunnelDirection(tile) != dir ||
 		GetTileZ(tile) != z
 	);
+
+	return tile;
+}
+
+
+/** Retrieve the exit-tile of the vehicle from inside a tunnel
+ * Very similar to GetOtherTunnelEnd(), but we use the vehicle's
+ * direction for determining which end of the tunnel to find
+ * @param v the vehicle which is inside the tunnel and needs an exit
+ * @return the exit-tile of the tunnel based on the vehicle's direction */
+TileIndex GetVehicleOutOfTunnelTile(const Vehicle *v)
+{
+	TileIndex tile = TileVirtXY(v->x_pos, v->y_pos);
+	DiagDirection dir = DirToDiagDir(v->direction);
+	TileIndexDiff delta = TileOffsByDir(dir);
+	uint z = v->z_pos;
+
+	dir = ReverseDiagDir(dir);
+	while (
+		!IsTunnelTile(tile) ||
+		GetTunnelDirection(tile) != dir ||
+		GetTileZ(tile) != z
+	) {
+		tile += delta;
+	}
 
 	return tile;
 }

tunnelbridge_cmd.c
@@ -1425,29 +1425,6 @@ static uint32 VehicleEnter_TunnelBridge(Vehicle *v, TileIndex tile, int x, int y
 	return 0;
 }
 
-/** Retrieve the exit-tile of the vehicle from inside a tunnel
- * Very similar to GetOtherTunnelEnd(), but we use the vehicle's
- * direction for determining which end of the tunnel to find
- * @param v the vehicle which is inside the tunnel and needs an exit
- * @return the exit-tile of the tunnel based on the vehicle's direction */
-TileIndex GetVehicleOutOfTunnelTile(const Vehicle *v)
-{
-	TileIndex tile = v->tile;
-	DiagDirection dir = DirToDiagDir(v->direction);
-	TileIndexDiff delta = TileOffsByDir(dir);
-	byte z = v->z_pos;
-
-	dir = ReverseDiagDir(dir);
-	while (
-		!IsTunnelTile(tile) ||
-		GetTunnelDirection(tile) != dir ||
-		GetTileZ(tile) != z
-	) {
-		tile += delta;
-	}
-
-	return tile;
-}
 
 const TileTypeProcs _tile_type_tunnelbridge_procs = {
 	DrawTile_TunnelBridge,					/* draw_tile_proc */